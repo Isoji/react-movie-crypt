f8ca45384d647ac5db221e19606362e8a3a8088f
Isoji <tremblayben@hotmail.ca>
2022-06-24 10:08:53 -0400

    Add test omdb API functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "https://www.omdbapi.com?apikey=d3960d27";
+
+const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('shre');
+    }, []);
+
+    return (
+        <div className='app'>
+            <h1>Movie Crypt</h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search'
+                    value='Shrek'
+                    onChange={() => { }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
