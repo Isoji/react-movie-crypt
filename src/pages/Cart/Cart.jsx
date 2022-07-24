5764bdd0949438c75bce418857b848a8645bbc13
Isoji <tremblayben@hotmail.ca>
2022-07-24 13:43:28 -0400

    Add basic Cart page displayed elements

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./Cart.scss";
 
 const Cart = () => {
-  const location = useLocation();
-  const { movie } = location.state;
+  const movie = useLocation().state.movie;
 
   return (
-    <div className="cart-container">
-      <h2>{movie.Title}</h2>
-      <h3>{movie.Year}</h3>
+    <div className="cart">
+      <div className="product__container">
+        <div className="product__container__content">
+          <img
+            className="product__container__content__poster"
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://www.movienewz.com/img/films/poster-holder.jpg"
+            }
+            alt="movie poster"
+          ></img>
+          <p>{movie.Title}</p>
+        </div>
+      </div>
+
+      <div className="subtotal__container">
+        <div className="subtotal__container__content">
+          <p>Total: 0.01 ETH</p>
+        </div>
+      </div>
     </div>
   );
 };
