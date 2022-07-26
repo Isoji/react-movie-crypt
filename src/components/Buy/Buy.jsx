7eac31cbf1b8ac9954eef3dd3390e43718f6b1da
Isoji <tremblayben@hotmail.ca>
2022-07-26 14:04:07 -0400

    Add toast notification to Buy component

--- src/components/Buy/Buy.jsx
+++ src/components/Buy/Buy.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import toast from "react-hot-toast";
 import "./Buy.scss";
 
+const notify = () =>
+  toast("Work in progress..", { style: { background: "#ba9511" } });
+
 const Buy = () => {
-  return <button className="buy-button">Buy Movie</button>;
+  return (
+    <button className="buy-button" onClick={notify}>
+      Buy Movie
+    </button>
+  );
 };
 
 export default Buy;
